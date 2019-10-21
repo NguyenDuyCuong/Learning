8aef513976d17762c944301f874620c39d191f7b
NguyenDuyCuong <cuongnd.ami@outlook.com>
2019-10-21 22:58:41 +0700

    regex python

--- Python/myRegex.py
+++ Python/myRegex.py
@@ -19,3 +19,12 @@ print(x)
 str = "The rain in Spain"
 x = re.sub("\s", "9", str, 2)
 print(x)
+
+# .span() returns a tuple containing the start-, and end positions of the match.
+# .string returns the string passed into the function
+# .group() returns the part of the string where there was a match
+str = "The rain in Spain"
+x = re.search(r"\bS\w+", str)
+print(x.span())
+print(x.string)
+print(x.group())
