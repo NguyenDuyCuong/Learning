2ed5d53f3533e32f8bc05974acf335e3dd14442b
Nguyễn Duy Cường <cuongnd.ami@outlook.com>
2022-12-09 23:40:01 +0700

    Exercise

---
+++ performace/DrawSquare/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Draw Square!");
+DrawSquare(100,100,50,5);
+Console.WriteLine("Done!");
+
+static void DrawSquare(int x, int y, int w, int h){
+    int xw = x + w;
+    int yh = y + h;
+
+    DrawLine(x,y,xw,y);
+    DrawLine(xw,y,xw,yh);
+    DrawLine(xw,yh,x,yh);
+    DrawLine(x,yh,x,y);
+}
+
+static void DrawLine(int x1, int y1, int x2, int y2){
+    // TODO: 
+    DrawSquare(x1,y1,x2,y2);
+}
---
+++ performace/Exercise/Program.cs
@@ -0,0 +1,15 @@
+﻿var results = GetCharacterCount("John Doe");
+foreach( KeyValuePair<char, int> kvp in results )
+{
+    Console.WriteLine("'{0}':{1}", kvp.Key, kvp.Value);
+}
+
+IDictionary<char, int> GetCharacterCount(string name)
+{
+    var result = new Dictionary<char, int>();
+    foreach (char c in name)
+    {
+        result[c] = 1;
+    }
+    return result;
+}
